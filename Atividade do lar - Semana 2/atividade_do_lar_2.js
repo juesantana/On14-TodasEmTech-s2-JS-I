3c79a7a3c7b22fb274377a0d9f17e22db6b38f5c
juliana-santana <juesantana@gmail.com>
2021-08-28 19:29:06 -0300

    atividade semana 2 juliana

---
+++ Atividade do lar - Semana 2/atividade_do_lar_2.js
@@ -0,0 +1,24 @@
+let texto = "Seja bem vindo ao meu site";
+alert(texto);
+ 
+ let numero1 = 87;
+ let numero2 = 96;
+
+ let soma = numero1+numero2;
+ alert(soma)
+
+
+ 
+ let numero3 = 8;
+ let numero4 = 5;
+ let multiplica = numero3*numero4;
+ alert(multiplica)
+
+
+ let lista_mercado = ['pão','uva','ovos','legumes','morangos','lentilha']
+alert(lista_mercado)
+
+
+
+
+
